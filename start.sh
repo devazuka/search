333ac541b77037428d9b0f8abd85324ed6a69367
Clement Denis <clement@01talent.com>
2024-08-22 01:03:50 +0100

    add the dump

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@
 export MEILI_HTTP_ADDR="localhost:$PORT"
 export MEILI_NO_ANALYTICS=true
 export MEILI_ENV=development # or production 
+export MEILI_IGNORE_MISSING_DUMP=true
+export MEILI_IGNORE_DUMP_IF_DB_EXISTS=true
+export MEILI_IMPORT_DUMP='20240821-235321640.dump'
+
 meilisearch
